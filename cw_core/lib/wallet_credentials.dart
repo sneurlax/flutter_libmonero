f2fc0b76bd9236967017720b34d806bef155726f
sneurlax <sneurlax@gmail.com>
2022-10-12 12:48:13 -0500

    add optional nettype param to WalletInfo type

--- cw_core/lib/wallet_credentials.dart
+++ cw_core/lib/wallet_credentials.dart
@@ -1,10 +1,12 @@
 import 'package:cw_core/wallet_info.dart';
 
 abstract class WalletCredentials {
-  WalletCredentials({this.name, this.password, this.height, this.walletInfo});
+  WalletCredentials(
+      {this.name, this.password, this.height, this.walletInfo, this.nettype});
 
   final String? name;
   final int? height;
   String? password;
   WalletInfo? walletInfo;
+  int? nettype;
 }
