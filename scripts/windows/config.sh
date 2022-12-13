f3588673dabd3ad32fbb8116894dc1b256866eaa
sneurlax <sneurlax@gmail.com>
2022-12-13 10:32:26 -0600

    add cross compilation environment variables

--- scripts/windows/config.sh
+++ scripts/windows/config.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 export API=21
-export WORKDIR="$(pwd)/"build
+export SCRIPTDIR="$(pwd)"
+export WORKDIR=${SCRIPTDIR}/build
+export CACHEDIR=${SCRIPTDIR}/cache
 export ANDROID_NDK_ZIP=${WORKDIR}/android-ndk-r17c.zip
 export ANDROID_NDK_ROOT=${WORKDIR}/android-ndk-r17c
 export ANDROID_NDK_HOME=$ANDROID_NDK_ROOT
@@ -16,3 +18,10 @@ if [ -z "$IS_ARM" ]; then
 else
   export TYPES_OF_BUILD="aarch64"
 fi
+export PREFIX=$WORKDIR/prefix_${TYPES_OF_BUILD}
+
+export CC=x86_64-w64-mingw32.static-gcc
+export CXX=x86_64-w64-mingw32.static-g++
+export HOST=x86_64-w64-mingw32
+export CROSS_COMPILE=x86_64-w64-mingw32.static-
+export CROSS=x86_64-w64-mingw32.static-
