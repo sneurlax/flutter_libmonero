27f6fd0069bf964992da3a67dfc07770cb613219
sneurlax <sneurlax@gmail.com>
2022-12-12 19:40:48 -0600

    udpate libsodium and add CROSS to make

--- scripts/windows/build_sodium.sh
+++ scripts/windows/build_sodium.sh
@@ -2,21 +2,19 @@
 
 . ./config.sh
 SODIUM_SRC_DIR=${WORKDIR}/libsodium
-SODIUM_BRANCH=1.0.16
+SODIUM_BRANCH=1.0.18 # See https://users.rust-lang.org/t/cross-compilation-linux-windows-pthread-linking-issues/50824/5 and https://github.com/jedisct1/libsodium/issues/962
 
 cd $WORKDIR
 rm -rf $SODIUM_SRC_DIR
 git clone https://github.com/jedisct1/libsodium.git $SODIUM_SRC_DIR -b $SODIUM_BRANCH
 cd $SODIUM_SRC_DIR
+
 ./autogen.sh
-CC=x86_64-w64-mingw32-gcc
-CXX=x86_64-w64-mingw32-g++
-HOST=x86_64-w64-mingw32
-CROSS_COMPILE="x86_64-w64-mingw32.static-"
+export CFLAGS='-Os'
 ./configure \
 	--prefix=${PREFIX} \
 	--host=${HOST} \
 	--enable-static \
 	--disable-shared
-make -j$THREADS
+make CROSS=x86_64-w64-mingw32.static- -j$THREADS
 make install
