2e9c2ad35797e4cde32107f1a7eca1f6fd39305f
sneurlax <sneurlax@gmail.com>
2022-12-13 11:09:54 -0600

    update iconv script

--- scripts/windows/build_iconv.sh
+++ scripts/windows/build_iconv.sh
@@ -2,10 +2,14 @@
 
 . ./config.sh
 
-export ICONV_FILENAME=libiconv-1.16.tar.gz
-export ICONV_SRC_DIR=$WORKDIR/libiconv-1.16
+ICONV_VERSION=1.16
 ICONV_SHA256="e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04"
-export ICONV_FILE_PATH=$CACHEDIR/$ICONV_FILENAME
+ICONV_FILENAME="libiconv-${ICONV_VERSION}.tar.gz"
+ICONV_SRC_DIR="${WORKDIR}/libiconv-${ICONV_VERSION}"
+ICONV_FILE_PATH=$CACHEDIR/$ICONV_FILENAME
+
+echo $ICONV_FILENAME
+echo $ICONV_FILE_PATH
 
 if [ ! -e "$ICONV_FILE_PATH" ]; then
   curl http://ftp.gnu.org/pub/gnu/libiconv/$ICONV_FILENAME -o $ICONV_FILE_PATH
@@ -17,10 +21,12 @@ cd $WORKDIR
 rm -rf $ICONV_SRC_DIR
 tar -xzf $ICONV_FILE_PATH -C $WORKDIR
 cd $ICONV_SRC_DIR
+
 ./configure \
-	--build=${HOST} \
 	--host=${HOST} \
 	--prefix=${PREFIX} \
+	--enable-static \
+	--disable-shared \
 	--disable-rpath
 make -j$THREADS
 make install
