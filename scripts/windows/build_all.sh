8add20f4b005a48e637524c0e12e6ef03aefc054
sneurlax <sneurlax@gmail.com>
2022-12-13 10:54:43 -0600

    separate zlib and openssl scripts

--- scripts/windows/build_all.sh
+++ scripts/windows/build_all.sh
@@ -3,6 +3,7 @@
 mkdir build
 ./build_iconv.sh
 ./build_boost.sh
+./build_zlib.sh
 ./build_openssl.sh
 ./build_sodium.sh
 ./build_expat.sh
---
+++ scripts/windows/build_zlib.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+. ./config.sh
+
+ZLIB_DIR=$WORKDIR/zlib
+ZLIB_TAG=v1.2.11
+ZLIB_COMMIT_HASH="cacf7f1d4e3d44d871b605da3b647f07d718623f"
+
+rm -rf $ZLIB_DIR
+git clone -b $ZLIB_TAG --depth 1 https://github.com/madler/zlib $ZLIB_DIR
+cd $ZLIB_DIR
+git reset --hard $ZLIB_COMMIT_HASH
+./configure --static
+make
