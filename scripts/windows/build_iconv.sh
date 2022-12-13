ef0a7c59482fd442c52a51bc50cb4838335b433c
sneurlax <sneurlax@gmail.com>
2022-12-13 11:00:46 -0600

    use cache where applicable

--- scripts/windows/build_iconv.sh
+++ scripts/windows/build_iconv.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 . ./config.sh
+
 export ICONV_FILENAME=libiconv-1.16.tar.gz
-export ICONV_FILE_PATH=$WORKDIR/$ICONV_FILENAME
 export ICONV_SRC_DIR=$WORKDIR/libiconv-1.16
 ICONV_SHA256="e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04"
+export ICONV_FILE_PATH=$CACHEDIR/$ICONV_FILENAME
 
 if [ ! -e "$ICONV_FILE_PATH" ]; then
   curl http://ftp.gnu.org/pub/gnu/libiconv/$ICONV_FILENAME -o $ICONV_FILE_PATH
