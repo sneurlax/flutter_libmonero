97bc8de9044b2f74c86027d653ee88fe4ea0f822
sneurlax <sneurlax@gmail.com>
2022-12-12 19:42:09 -0600

    use unbound fork and use PIC
    see https://github.com/NLnetLabs/unbound/issues/807 and https://github.com/NLnetLabs/unbound/pull/808

--- scripts/windows/build_unbound.sh
+++ scripts/windows/build_unbound.sh
@@ -14,20 +14,16 @@ UNBOUND_VERSION=release-1.17.0
 UNBOUND_HASH="d25e0cd9b0545ff13120430c94326ceaf14b074f"
 UNBOUND_SRC_DIR=$WORKDIR/unbound-1.17.0
 '
-UNBOUND_VERSION=release-1.16.2
-UNBOUND_HASH="cbed768b8ff9bfcf11089a5f1699b7e5707f1ea5"
-UNBOUND_SRC_DIR=$WORKDIR/unbound-1.16.2
+UNBOUND_VERSION=fix/1.17.0
+UNBOUND_HASH="0b5dd63b7417601448acc2e2b9ff2d8dd07ad31f"
+UNBOUND_SRC_DIR=$WORKDIR/unbound-1.17.0
 
 cd $WORKDIR
 rm -rf $UNBOUND_SRC_DIR
-git clone https://github.com/NLnetLabs/unbound.git -b ${UNBOUND_VERSION} ${UNBOUND_SRC_DIR}
+git clone https://github.com/cypherstack/unbound.git -b ${UNBOUND_VERSION} ${UNBOUND_SRC_DIR} # See https://github.com/NLnetLabs/unbound/pull/808; once merged, use NLNetLabs/unbound instead of cypherstack/unbound#fix/windows
 cd $UNBOUND_SRC_DIR
 test `git rev-parse HEAD` = ${UNBOUND_HASH} || exit 1
 
-CC=x86_64-w64-mingw32-gcc
-CXX=x86_64-w64-mingw32-g++
-HOST=x86_64-w64-mingw32
-CROSS_COMPILE="x86_64-w64-mingw32.static-"
 ./configure \
 	CFLAGS=-fPIC \
 	CXXFLAGS=-fPIC \
@@ -37,9 +33,8 @@ CROSS_COMPILE="x86_64-w64-mingw32.static-"
 	--enable-static \
 	--disable-shared \
 	--disable-flto \
-	--enable-static-openssl \
-	--with-openssl-includes=${PREFIX} \
+	--with-pic \
 	--with-ssl=${PREFIX} \
 	--with-libexpat=${PREFIX}
-make -j$THREADS
-make -j$THREADS install
+make CROSS=x86_64-w64-mingw32.static- -j$THREADS
+make install
