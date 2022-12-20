cf3de02e6a3354a9f34c8817fbe3ce2abfe94709
sneurlax <sneurlax@gmail.com>
2022-12-20 10:58:41 -0600

    update zlib script for mxe

--- scripts/windows/build_zlib.sh
+++ scripts/windows/build_zlib.sh
@@ -12,5 +12,26 @@ rm -rf $ZLIB_DIR
 git clone -b $ZLIB_TAG --depth 1 https://github.com/madler/zlib $ZLIB_DIR
 cd $ZLIB_DIR
 git reset --hard $ZLIB_COMMIT_HASH
-./configure --static
+
+sed 's/PREFIX =/PREFIX = x86_64-w64-mingw32.static-/' -i win32/Makefile.gcc
+
+HOST=x86_64-w64-mingw32
+CC=x86_64-w64-mingw32.static-gcc
+: '
+./configure \
+	--static \
+	--prefix=${PREFIX}
 make
+make install
+'
+make -f win32/Makefile.gcc
+	DESTDIR=${PREFIX} \
+	BINARY_PATH=/usr/x86_64-w64-mingw32/bin \
+	INCLUDE_PATH=/usr/x86_64-w64-mingw32/include \
+	LIBRARY_PATH=/usr/x86_64-w64-mingw32/lib \
+	PREFIX=x86_64-w64-mingw32.static- \
+	make -f win32/Makefile.gcc install # See https://stackoverflow.com/a/26021820
+cp -r ${PREFIX}/usr/x86_64-w64-mingw32/* ${PREFIX}
+rm -rf ${PREFIX}/usr # 👻
+
+# See https://stackoverflow.com/questions/21322707/zlib-header-not-found-when-cross-compiling-with-mingw
